a4437ac584051df578fff6092ab816ecede0b735
SubhajitBar <pvt.subha@gmail.com>
2023-04-22 23:00:11 +0530

    route.put to cancelEnroll

--- routes/enrollmentRoutes.js
+++ routes/enrollmentRoutes.js
@@ -20,7 +20,7 @@ router.route("/admin/deleteenrollment/:id").delete(isAuthenticated, authorizeAdm
 
 
 // cancel user enrollment 
-router.route("/admin/canceluserenrollment/:id").post(isAuthenticated, authorizeAdmin , cancelEnrollment );
+router.route("/admin/canceluserenrollment/:id").put(isAuthenticated, authorizeAdmin , cancelEnrollment );
 // get all enrolled user
 router.route("/admin/getallenrolledstudents").get(isAuthenticated, authorizeAdmin , getAllEnrolledStudents );
 
