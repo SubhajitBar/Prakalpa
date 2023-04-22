0a9bcf4ad9023d1b9c06af937f7b0b7b1fa4ad68
SubhajitBar <pvt.subha@gmail.com>
2023-04-22 17:42:56 +0530

    enrollment testing

--- controllers/enrollmentController.js
+++ controllers/enrollmentController.js
@@ -90,15 +90,15 @@ export const enrollmentVerification = catchAsyncError(async (req, res, next) => 
     
     await user.save();
 
-    res.redirect(
-        `${process.env.FRONTEND_URL}/project/${user.enrolledProjectId}`
-      );
+    // res.redirect(
+    //     `${process.env.FRONTEND_URL}/project/${user.enrolledProjectId}`
+    //   );
 
-    // res.status(200).json({
-    //     success: true,
-    //     message: `Your enrollment is: ${user.enrollmentStatus} for project ${user.enrolledProjectId}`
+    res.status(200).json({
+        success: true,
+        message: `Your enrollment is: ${user.enrollmentStatus} for project ${user.enrolledProjectId}`
 
-    // });
+    });
 
 });
 
