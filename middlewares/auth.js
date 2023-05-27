227c265ba7a80d829005aabd195d1fab6ce3147c
SubhajitBar <pvt.subha@gmail.com>
2023-05-27 22:38:51 +0530

    auth updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,13 @@ import ErrorHandler from "../utils/errorHandler.js";
 import { Course } from "../models/Course.js";
 import { Guides } from "../models/Guides.js";
 
-
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
 
   if (!token) return next(new ErrorHandler("Not logged in", 401));
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = await User.findById(decoded._id);
+  req.user =
+    (await User.findById(decoded._id)) || (await Guides.findById(decoded._id));
   next();
 });
 
@@ -23,7 +23,6 @@ export const isGuideAuthenticated = catchAsyncError(async (req, res, next) => {
   req.user = await Guides.findById(decoded._id);
   next();
 });
- 
 
 export const authorizeAdmin = (req, res, next) => {
   if (req.user.role !== "admin")
@@ -46,19 +45,43 @@ export const authorizeSubscribers = (req, res, next) => {
   next();
 };
 
-export const authorizeStudent  = async(req,res,next)=>{
+export const authorizeStudent = async (req, res, next) => {
   const project = await Course.findById(req.params.id);
 
-  if (req.user.role !== "admin" && req.user.enrolledProjectId !== project.id){
-    return next(new ErrorHandler(`Only enrolled students can access this resource`, 403));
+  if (req.user.role !== "admin") {
+    if (
+      req.user.enrolledProjectId !== undefined &&
+      req.user.enrolledProjectId !== project.id
+    ) {
+      return next(
+        new ErrorHandler(`Only enrolled students can access this resource`, 403)
+      );
+    } 
+    
+    if (
+      req.user.assignedProjectId !== undefined &&
+      req.user.assignedProjectId !== project.id
+    ) {
+      return next(
+        new ErrorHandler(`Only assign guide can access this resource`, 403)
+      );
+    } 
+    
   }
 
-  next();
+  // if (req.user.role !== "admin" && (req.user.enrolledProjectId !== project.id || req.user.assignedProjectId !== project.id)){
+  //   console.log(req.user.role);
+  //   console.log(project.id);
+  //   console.log(req.user.enrolledProjectId);
+  //   console.log(req.user.assignedProjectId);
+  //   return next(new ErrorHandler(`Only enrolled students can access this resource`, 403));
+  // }
 
+  next();
 };
 
 // export const isProjectMatched = catchAsyncError(async (req,res,next)=>{
-  
+
 //   const project = await Course.findById(req.params.id);
 //   if (req.user.enrolledProjectId === project.id){
 //    next();
